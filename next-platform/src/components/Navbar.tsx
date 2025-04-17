85089c187fcd649a8ac9879afd9df81c8668c0ec
Wana Pichpongsa <wana@pichpongsa.com>
2025-04-17 17:38:55 +0800

    OOPS: Navbar

---
+++ next-platform/src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 right-0 p-4 z-20">
+      <div className="container mx-auto flex justify-center">
+        <div className="flex items-center gap-3 bg-gray-800/80 backdrop-blur-sm rounded-full px-4 py-2 shadow-lg border border-gray-700/50">
+          <a href="/" className="flex items-center gap-3 text-zinc-50">
+              Home
+          </a>
+          <a href="/document" className="flex items-center gap-3 text-zinc-50">
+            File
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
