339213174d1596d11999c9ff3c59b8f1b48a297d
Wana Pichpongsa <wana@pichpongsa.com>
2025-04-17 17:35:46 +0800

    BOILER: python api comments (will delete py code in -b main)

--- next-platform/src/app/api/now-engine/route.ts
+++ next-platform/src/app/api/now-engine/route.ts
@@ -1,25 +1,40 @@
 "use server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: Request) {
+/*
+API routes that we need:
+Proxy: Redis server cache
+1. Load pre-existing conversation states.
+3. Send and return chat input messages.
+5. File buffer local storage and download.
+
+Condition: Modular & distinct i.e., seperate files/functions, explicit purpose
+How to do it: Make distinct slugs for distinct roles. Naming it now-engine was a rookie mistake
+
+Skepticism about this API route design:
+1. Not explicit in what it does
+*/
+
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
-
     const endpoint = "http://127.0.0.1:8080/now-engine"; // python backend
     const { message } = await req.json(); // parse message from json req
     const response = await fetch(endpoint, {
     method: "POST",
     headers: {"Accept": "application/json"},
-    body: message,
+    body: message, // <- I believe this is a problem because body would be a nested JSON?
     });
     if (!response.ok) throw new Error("Python backend failure");
+    
+    // no need to await response.json? (Think architecture diagram)
 
     return NextResponse.json(
-      { success: true, reply: response },
+      { reply: response },
       { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
-      { success: false, error: 'Error in chat API: ' + error }, 
+      { error: 'Error in chat API: ' + error }, 
       { status: 500 } // perhaps be proxy for python message as 500 means absolute failure?
     );
   }
