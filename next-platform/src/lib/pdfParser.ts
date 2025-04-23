3b2329948b3cc0e5fe308082378027f9a7883dd2
Wana Pichpongsa <wana@pichpongsa.com>
2025-04-23 14:55:25 +0700

    pdfParser

---
+++ next-platform/src/lib/pdfParser.ts
@@ -0,0 +1,39 @@
+"use server";
+
+/* 
+1. Transmission from client to filesystem? Or as buffer to server handlers?
+2. Pdf-to-text is very hard rn, but pdf-to-vector should be fine?
+
+langchain document_loaders: https://js.langchain.com/docs/integrations/document_loaders/file_loaders/pdf/
+
+pdftotextjs --layout option to preserve layout. Adobe works but paid.
+Source: https://www.reddit.com/r/node/comments/186y7y0/looking_for_a_good_pdfparser_to_extract_text_any/
+*/
+
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+
+export async function getPDFChunks() {
+  try {
+  const bankStatementPath = process.env.PDF_PATH!;
+
+  // make fn && try catch
+  const loader = new PDFLoader(bankStatementPath); // can {parsedItemSeperator: "",} for no spaces
+  const docs = await loader.load(); // also has .metadata()
+
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const splitDocs = await textSplitter.splitDocuments(docs);
+  console.log(splitDocs[0].pageContent);
+
+  } catch (error) {
+    console.error("error: " + error);
+    throw new Error("PDF chunking failed")
+  }
+}
+
+getPDFChunks();
